9f77983234fb20e855e0aebe7953d63a3b09f5bf
unknown <sam.jeyarajn@intern.asirtech.com>
2023-05-25 03:26:22 -0700

    ffttt

---
+++ scan.java
@@ -0,0 +1,15 @@
+package input;
+
+import java.util.Scanner;
+
+public class scan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner ob=new Scanner(System.in);
+     System.out.println("enter username:");
+     String name=ob.nextLine();
+     System.out.println("username is: "+name);
+	}
+
+}
